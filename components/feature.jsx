80a073ac4af72cb647698eb133425e70cee3f555
alfonsocv12 <alfonsocvu@gmail.com>
2021-04-25 01:38:25 -0600

    Start with this docs page

---
+++ components/feature.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Feature = ( props ) => {
+  return (
+    <div>
+      <p className="text-lg mb-2 text-gray-600 md:text-xl">Python module manager</p>
+    </div>
+  )
+}
+export default Feature
