23d401650647fe3671e308bafeaa9877f5bad2dd
alfonsocv12 <alfonsocvu@gmail.com>
2021-04-25 11:58:17 -0600

    Finishing quickstart

--- components/subtitle.jsx
+++ components/subtitle.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Subtitle = ( props ) => {
+  let title = (props.title ? props.title : 'Python module manager')
   return (
     <div>
-      <p className="text-lg mb-2 text-gray-600 md:text-xl">Python module manager</p>
+      <p className="text-lg mb-2 text-gray-600 md:text-xl">{title}</p>
     </div>
   )
 }
