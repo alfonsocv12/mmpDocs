8f75254fc4ccb43c897a858901f511fd3c2e0f9d
alfonsocv12 <alfonsocvu@gmail.com>
2021-04-27 16:37:39 -0600

    Change meto to go to mmp.alfonsocvu.me

--- theme.config.js
+++ theme.config.js
@@ -35,17 +35,17 @@ export default {
 	  <meta name="msapplication-config" content="/browserconfig.xml"/>
 
     //Twitter
-    <meta name="twitter:site" content="@mmp-y"/>
+    <meta name="twitter:site" content="@mmp.alfonsocvu.me"/>
     <meta name="twitter:creator" content="alfonso102"/>
     <meta name="twitter:card" content="summary_large_image"/>
 
 
     //Open Graph / Facebook
     <meta property="og:type" content="website"/>
-    <meta property="og:url" content="https://www.mmp-y.com/"/>
+    <meta property="og:url" content="https://mmp.alfonsocvu.me/"/>
     <meta property="og:site_name" content="mmp docs"/>
     <meta property="og:title" content="mmp - Module Manager Python"/>
     <meta property="og:description" content="This is the mmp documentation page, where you will find our quick start and the full explanation of all the commands of the cli"/>
-    <meta property="og:image" content="https://www.mmp-y.com/owl.png"/>
+    <meta property="og:image" content="https://mmp.alfonsocvu.me/owl.png"/>
   </>
 }
